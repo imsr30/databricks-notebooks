2f636d1eb1cd583c599eb1f41cab7e9e9eb00aab
imsr30 <59094794+imsr30@users.noreply.github.com>
2022-07-09 08:01:07 +0530

    Delete scripts directory

--- scripts/install-sql-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# https://www.npmjs.com/package/sql-cli
-# install node (if not yet)
-# brew install node
-
-npm install -g sql-cli
--- scripts/setup-cosmosdb-feed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x -e
-
-# https://github.com/tknandu/TwitterCosmosDBFeed
-
-git clone https://github.com/tknandu/TwitterCosmosDBFeed.git
-pip install tweepy==3.3.0
-pip install pyDocumentDB
